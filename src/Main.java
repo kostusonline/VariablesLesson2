99ef5c440cef45eb469a738656b859b427e62574
Konstantin Terskikh <kostus.online@gmail.com>
2024-09-30 20:58:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Konstantin Terskikh, kostus.online@gmail.com, 2024
+// SkyPro, Java Developer
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
